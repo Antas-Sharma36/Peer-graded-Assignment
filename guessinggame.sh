212c71b7c23434b619369abbc29402b8ed2414c5
Antas-Sharma36 <sharmaantas20@gmail.com>
2022-12-31 18:29:33 +0530

    Done

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+total_files=$(ls | wc -l)
+# And for the beginning take -
+guessed_no=$(($total_files+1))
+function guessinggame {
+while [[ $guessed_no -ne $total_files ]]
+do
+   echo "Guess the number of files - "
+   read guessed_no
+   if [[ $guessed_no -gt $total_files ]]
+   then
+        echo "Your guess is greater than the actual number of files"
+        echo "Try Again!"
+   elif [[ $guessed_no -lt $total_files ]]
+   then
+        echo "Your guess is smaller than the actual number of files"
+        echo "Try Again!"
+   else
+        echo "Congrats! You got it right"
+   fi
+done
+}
+guessinggame
